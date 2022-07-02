b03d36083aca8930b47777793130fd783d5b88d9
srezal <sereshka0967@gmail.com>
2022-07-02 05:43:32 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import scapy.all as scapy
+from scapy.layers import http
+
+
+def sniff(interface):
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
+
+
+def process_sniffed_packet(packet):
+    if packet.haslayer(http.HTTPRequest):
+        if packet.haslayer(scapy.Raw):
+            print(packet[scapy.Raw].load.decode('utf-8'))
+
+
+sniff("eth0")
